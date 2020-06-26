a7924700020f89aa5e557e684d7b72714fa2d1d4
ts <tristansetha@gmail.com>
2020-06-26 14:23:07 -0700

    react hook form install

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import './Login.scss';
 export default function Login() {
+  
+  const [value, setValue] = useState({
+    username: ''
+  });
+
   return (
-    <div>
-      
+    <div className="login-container"> 
+      <div className="login-inner">
+        <h1>Login: </h1>
+        <form>
+        <input className="login-username"></input>
+
+        </form>
+      </div>
     </div>
   )
-}
+} 
